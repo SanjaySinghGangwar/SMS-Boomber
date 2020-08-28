f67e31ed11143eae45e0e57c25691dc762b8fca8
hgnis <gangwar.jar@gmail.com>
2020-08-28 20:24:27 +0530

    Functionality Done

---
+++ app/src/main/java/com/sanjaysgangwar/smsboomber/service/myProgressDialog.java
@@ -0,0 +1,33 @@
+package com.sanjaysgangwar.smsboomber.service;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.sanjaysgangwar.smsboomber.R;
+
+public class myProgressDialog {
+    private Activity contexT;
+    private AlertDialog dialog;
+
+    public myProgressDialog(Activity mContext) {
+        contexT = mContext;
+    }
+
+    public void show() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(contexT);
+        LayoutInflater inflater = contexT.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.progress_dialog, null));
+        builder.setCancelable(false);
+        dialog = builder.create();
+        dialog.show();
+
+    }
+
+    public void dismiss() {
+        dialog.dismiss();
+
+    }
+}
